a3cdf891a59feb051c9ddbb21f7ae921eb731df4
Philippe Mourey <philippe@mourey.com>
2022-12-04 09:38:33 +0100

    Day 4

---
+++ 2022/Day4/day4_part2.py
@@ -0,0 +1,17 @@
+import os
+from typing import List
+
+if __name__ == "__main__":
+    file_name = os.path.abspath(".") + "/input.txt"
+    with open(file_name) as file:
+        assignement_pairs = 0
+        for line in file:
+            range_1, range_2 = line.split(',')
+            start_1, end_1 = map(int, range_1.split('-'))
+            start_2, end_2 = map(int, range_2.split('-'))
+            set_1 = set(range(start_1, end_1 + 1))
+            set_2 = set(range(start_2, end_2 + 1))
+            if set_1.intersection(set_2):
+                assignement_pairs += 1
+                # print(range_1, range_2, end='')
+        print(assignement_pairs)
---
+++ 2022/README.md
@@ -0,0 +1,4 @@
+##https://adventofcode.com/2022
+
+###Some ressources:
+https://docs.python.org/3.5/library/stdtypes.html#set
