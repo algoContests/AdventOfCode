19fd46a46c4d0010eb6cbda5eaf2fc7a099de24a
Philippe Mourey <philippe@mourey.com>
2023-12-09 17:24:49 +0100

    Update aoc_day7_part1.py

--- 2021/Day7/aoc_day7_part1.py
+++ 2021/Day7/aoc_day7_part1.py
@@ -1,1 +1,33 @@
-""" --- Day 7: The Treachery of Whales --- """import mathimport statisticscrabs = list(map(int, input().split(',')))crabs.sort()print(f'min: {min(crabs)} - max: {max(crabs)}')print(crabs)min_fuel = math.inffor pos in crabs:# for pos in range(max(crabs) + 1):    fuel = sum([abs(p - pos) for p in crabs])    if fuel < min_fuel:        min_fuel = fuel        best_pos = posmediane = (crabs[500] + crabs[501]) / 2print(f'mediane = {mediane}')print(f'other = {crabs[503]}')print(f'{len(crabs)} crabs - median : {statistics.median(crabs)}')print(f'best_pos = {best_pos} - min_fuel = {min_fuel}')fuel = 0for pos in crabs:    fuel += abs(mediane - pos)print(f'fuel = {fuel}')
+""" --- Day 7: The Treachery of Whales --- """
+import math
+import statistics
+
+crabs = list(map(int, input().split(',')))
+
+crabs.sort()
+print(f'min: {min(crabs)} - max: {max(crabs)}')
+print(crabs)
+
+min_fuel = math.inf
+for pos in crabs:
+# for pos in range(max(crabs) + 1):
+    fuel = sum([abs(p - pos) for p in crabs])
+    if fuel < min_fuel:
+        min_fuel = fuel
+        best_pos = pos
+
+mediane = (crabs[500] + crabs[501]) / 2
+print(f'mediane = {mediane}')
+print(f'other = {crabs[503]}')
+print(f'{len(crabs)} crabs - median : {statistics.median(crabs)}')
+print(f'best_pos = {best_pos} - min_fuel = {min_fuel}')
+
+fuel = 0
+for pos in crabs:
+    fuel += abs(mediane - pos)
+
+print(f'fuel = {fuel}')
+
+
+
+
