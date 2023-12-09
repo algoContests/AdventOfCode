6ed2390ac1043334a4c8937368a1cabe9bec3fd4
Philippe Mourey <philippe@mourey.com>
2023-12-09 17:20:13 +0100

    Update aoc_day6.py

--- 2020/Day6/aoc_day6.py
+++ 2020/Day6/aoc_day6.py
@@ -1,1 +1,17 @@
-yes_count = 0yes_response = {}while True:    try:        line = input()        if len(list(line)) == 0:            yes_count += len(yes_response)            yes_response = {}        else:            for car in list(line):                yes_response[car] = 1    except:        yes_count += len(yes_response)        breakprint(yes_count)
+
+yes_count = 0
+yes_response = {}
+while True:
+    try:
+        line = input()
+        if len(list(line)) == 0:
+            yes_count += len(yes_response)
+            yes_response = {}
+        else:
+            for car in list(line):
+                yes_response[car] = 1
+    except:
+        yes_count += len(yes_response)
+        break
+
+print(yes_count)
