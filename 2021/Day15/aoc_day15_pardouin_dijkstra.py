e8daa7ce60543e8b0233ff83064fe9fe540977b8
Philippe Mourey <philippe@mourey.com>
2023-12-09 17:27:04 +0100

    Update aoc_day15_pardouin_dijkstra.py

--- 2021/Day15/aoc_day15_pardouin_dijkstra.py
+++ 2021/Day15/aoc_day15_pardouin_dijkstra.py
@@ -1,1 +1,41 @@
-""" pardouinça c'est la version basique qui renvoit juste la distance du plus court cheminon peut faire une version plus complexe qui renvoie aussi le chemin lui-même"""from heapq import heappush, heappopdef dijkstra():    pqueue = [(0, sommet_depart)]    dist_dans_pqueue = {sommet_depart: 0}    deja_fait = set()    while pqueue:        dist, sommet = heappop(pqueue)        if sommet in deja_fait:            continue        deja_fait.add(sommet)        for s, d in voisins[sommet]:            if s == sommet_fin:                return dist + d            if s not in dist_dans_queue or dist_dans_pqueue[s] > d:                heappush(pqueue, (dist + d, s))                dist_dans_pqueue[s] = ddef dijkstra():    pqueue = [(0, sommet_depart)]    dist_dans_pqueue = {sommet_depart: 0}    deja_fait = set()    while pqueue:        dist, sommet = heappop(pqueue)        if sommet in deja_fait:            continue        deja_fait.add(sommet)        for s, d in voisins[sommet]:            nouv_d = dist + d            if s == sommet_fin:                return nouv_d            if s not in dist_dans_queue or dist_dans_pqueue[s] > nouv_d:                heappush(pqueue, (nouv_d, s))                dist_dans_pqueue[s] = nouv_d
+""" pardouin
+ça c'est la version basique qui renvoit juste la distance du plus court chemin
+on peut faire une version plus complexe qui renvoie aussi le chemin lui-même
+"""
+from heapq import heappush, heappop
+
+
+def dijkstra():
+    pqueue = [(0, sommet_depart)]
+    dist_dans_pqueue = {sommet_depart: 0}
+    deja_fait = set()
+
+    while pqueue:
+        dist, sommet = heappop(pqueue)
+        if sommet in deja_fait:
+            continue
+        deja_fait.add(sommet)
+        for s, d in voisins[sommet]:
+            if s == sommet_fin:
+                return dist + d
+            if s not in dist_dans_queue or dist_dans_pqueue[s] > d:
+                heappush(pqueue, (dist + d, s))
+                dist_dans_pqueue[s] = d
+
+def dijkstra():
+    pqueue = [(0, sommet_depart)]
+    dist_dans_pqueue = {sommet_depart: 0}
+    deja_fait = set()
+
+    while pqueue:
+        dist, sommet = heappop(pqueue)
+        if sommet in deja_fait:
+            continue
+        deja_fait.add(sommet)
+        for s, d in voisins[sommet]:
+            nouv_d = dist + d
+            if s == sommet_fin:
+                return nouv_d
+            if s not in dist_dans_queue or dist_dans_pqueue[s] > nouv_d:
+                heappush(pqueue, (nouv_d, s))
+                dist_dans_pqueue[s] = nouv_d
