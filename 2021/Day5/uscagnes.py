a259b78e03ac366e91d94fb6b490014a58753b57
Philippe Mourey <philippe@mourey.com>
2023-12-09 17:23:59 +0100

    Update uscagnes.py

--- 2021/Day5/uscagnes.py
+++ 2021/Day5/uscagnes.py
@@ -1,1 +1,7 @@
-import randominput = "Jphi Phil Hub Franck Christophe Jérôme JPaul Mario Fabio Marc"les_copains = input.split(' ')capitaine = "Hub"meilleur_joueur = random.choice(les_copains)meilleur_joueur = random.choice([meilleur_joueur] + [capitaine])print(meilleur_joueur)
+import random
+input = "Jphi Phil Hub Franck Christophe Jérôme JPaul Mario Fabio Marc"
+les_copains = input.split(' ')
+capitaine = "Hub"
+meilleur_joueur = random.choice(les_copains)
+meilleur_joueur = random.choice([meilleur_joueur] + [capitaine])
+print(meilleur_joueur)
