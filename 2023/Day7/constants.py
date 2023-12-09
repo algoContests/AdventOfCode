2a2f1c2966113053c886d8ac1780708fa188bc40
Philippe Mourey <philippe@mourey.com>
2023-12-09 16:36:24 +0100

    add AOC 2023 Day 7

---
+++ 2023/Day7/constants.py
@@ -0,0 +1,5 @@
+from functions import *
+
+card_values = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A']
+
+list_functions: List = [five_of_a_kind, four_of_a_kind, full_house, three_of_a_kind, two_pairs, one_pair, high_card]
