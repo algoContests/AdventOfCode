1a462e810590eb19799501a22c99cfb2877794e4
Philippe Mourey <philippe@mourey.com>
2023-12-09 17:20:31 +0100

    Update aoc_day_3.py

--- 2020/Day3/aoc_day_3.py
+++ 2020/Day3/aoc_day_3.py
@@ -1,1 +1,16 @@
-rows, cols = 323, 31x, y = (0, 0)trees_count = 0forest = []for _ in range(rows):    line = input()    tab = list(line)    while x > len(tab):        tab += tab    forest.append(tab)    if tab[x] == '#':        trees_count += 1    x += 3print(trees_count)
+rows, cols = 323, 31
+
+x, y = (0, 0)
+trees_count = 0
+forest = []
+for _ in range(rows):
+    line = input()
+    tab = list(line)
+    while x > len(tab):
+        tab += tab
+    forest.append(tab)
+    if tab[x] == '#':
+        trees_count += 1
+    x += 3
+
+print(trees_count)
